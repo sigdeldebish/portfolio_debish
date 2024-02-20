70a2bebc7aff5e5027935e5b436864593fd2de97
sigdeldebish <sigdel.bish@gmail.com>
2024-02-20 14:28:12 -0500

    Gallery Final

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,31 +1,59 @@
 let slideIndex = 0;
 showSlides();
 
-function plusSlides(n) {
-    clearTimeout(timer);
-    slideIndex += n - 1;
-    console.log(slides.length)
-    if (slideIndex < 0) { slideIndex = slides.length - 1; }
-    else if (slideIndex >= slides.length) { slideIndex = 0; }
-    console.log(slides.length)
-    showSlides();
-}
-
 
 function showSlides() {
     let slides = document.getElementsByClassName("image_slides");
-    console.log(slides)
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "grid";  
+    slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
-function resetTimer() {
-    clearTimeout(showSlides);
-    setTimeout(showSlides, 4000);
-}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let canvas = document.getElementById('doodlingCanvas');
+    let ctx = canvas.getContext('2d');
+    let reset = document.getElementById('reset_doodle');
+
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    ctx.strokeStyle = '#84308a';
+  
+    function startPosition(e) {
+      painting = true;
+      draw(e); 
+    }
+  
+    function endPosition() {
+      painting = false;
+      ctx.beginPath();
+    }
+
+    function resetCanvas() {
+        
+        ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetWidth);
+    }
+  
+    function draw(e) {
+      if (!painting) return;
+      ctx.lineWidth = 7;
+      ctx.lineCap = 'round'; 
+
+      ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', endPosition);
+    canvas.addEventListener('mousemove', draw);
+    reset.addEventListener('click', resetCanvas);
+  });
+
+
 
